11f65b6d676c57646b508042808d7391ea27cdda
mxlai <maxslie@gmail.com>
2015-03-20 00:55:51 -0400

    newGame function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,24 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Start a new game ---*/
+  	function newGame() {
+  		randomNumber();
+  	}
+
+  	$(".new").click(function(event) {
+  		console.log("New Game!");
+		newGame();	
+	});
+
+  	/*--- Generate a random number ---*/
+  	function randomNumber() {
+  		var number = Math.floor((Math.random() * 100) + 1);
+  		console.log(number);
+  	}
+
+  	/*Math.abs for absolute value
+  	userGuess - computerNumber > 10...*/
 });
 
 
